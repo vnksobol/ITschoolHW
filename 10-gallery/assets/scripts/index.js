8c5190955cf5839a2978bee90b57c90fc7634cb4
vnksobol <vnksobol@gmail.com>
2023-03-26 23:52:27 +0300

    1st commit

---
+++ 10-gallery/assets/scripts/index.js
@@ -0,0 +1,17 @@
+// function showMessage () {
+//     console.log("Я учу JavaScript!");
+// }
+
+// showMessage();
+
+const blockFoto = document.getElementById("img")
+// const blockFoto = document.getElementById("img"); 
+//     // console.log(blockFoto);
+
+function showLeftFoto() {
+    blockPhoto.slyle.backgroundImage =
+    `url('../images/02.jpg')`;
+}
+
+
+// "url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fru.pinterest.com%2Fpin%2F376261743866615679%2F&psig=AOvVaw1UmgLGxlPW_HDfT6SYNXBV&ust=1679694629994000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCLDVo6CE8_0CFQAAAAAdAAAAABAE')";
