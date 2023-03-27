ae8f38791f4e15b3d99f6495eab2b17b6893ef3c
vnksobol <vnksobol@gmail.com>
2023-03-27 03:10:13 +0300

    taxi fixed

--- 09Calculator/assets/index.js
+++ 09Calculator/assets/index.js
@@ -1,3 +1,34 @@
+x = 40000;
+y = 22000;
+console.log(x + y);
+
+console.log('Владивосток' + ' 2000');
+
+messenger = 128;
+photo = 254;
+article = 137;
+video = 201;
+let minutes = (messenger + photo + article + video);
+let minutesToHours = (minutes) / 60;
+console.log(`Всего Русалка прокрастинировала ${minutesToHours} часов.`);;
+
+console.log(2 ** 5);
+
+console.log('Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!'); 
+
+const greetings = "Привет";
+const userName = "Кот";
+console.log(greetings + ', ' + userName);
+
+let time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time -= 15;
+console.log('Немного сторис в соцсетях. Осталось минут: ' + time);
+time -= 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+
 let fahrenheit = 451;
 let celsius = (fahrenheit - 32) / 1.8;
 console.log(`${fahrenheit} градуса по Фаренгейту — это ${celsius} градуса по Цельсию.`);
