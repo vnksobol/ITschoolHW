3d2cbe2a275bc16d096319dd0ff4d8b6dddfb286
vnksobol <vnksobol@gmail.com>
2023-03-27 20:19:52 +0300

    2nd commit

--- 10-gallery/assets/scripts/index.js
+++ 10-gallery/assets/scripts/index.js
@@ -4,14 +4,19 @@
 
 // showMessage();
 
-const blockFoto = document.getElementById("img")
-// const blockFoto = document.getElementById("img"); 
-//     // console.log(blockFoto);
+const blockPhoto = document.getElementById("img"); 
+    console.log(blockPhoto);
 
-function showLeftFoto() {
+function showLeftPhoto(blockPhoto.slyle.backgroundImage) {
     blockPhoto.slyle.backgroundImage =
     `url('../images/02.jpg')`;
 }
-
-
-// "url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fru.pinterest.com%2Fpin%2F376261743866615679%2F&psig=AOvVaw1UmgLGxlPW_HDfT6SYNXBV&ust=1679694629994000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCLDVo6CE8_0CFQAAAAAdAAAAABAE')";
+    
+showLeftPhoto();
+    
+function showLeftPhoto() {
+    blockPhoto.slyle.backgroundImage =
+    `url('../images/03.jpg')`;
+}
+    
+showRightPhoto();
