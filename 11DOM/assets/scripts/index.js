1dda72edd8aa5b2a056298c0b8584061a42b66e3
vnksobol <vnksobol@gmail.com>
2023-04-03 20:40:22 +0300

    new selectors added

--- 11DOM/assets/scripts/index.js
+++ 11DOM/assets/scripts/index.js
@@ -5,7 +5,7 @@ let ageTable = document.querySelector('#age-table');
 console.log(ageTable);
 
 let ageTable2 = document.getElementById('age-table');
-console.log(ageTable);
+console.log(ageTable); 
 
 
 // 2. Все элементы `label` **внутри** этой таблицы
@@ -15,7 +15,11 @@ console.log(ageList);
 
 
 // 3. Форму `form` с именем `name="search-person"`
-let searchPerson = document.getElementByTagName('search-person');
+let searchPerson = document.getElementsByName('search-person'); //получить все такие формы
 console.log(searchPerson);
 
+let searchPerson2 = document.querySelector('[name="search-person"]'); 
+console.log(searchPerson2);
 
+let searchPerson3 = document.querySelectorAll('[name="search-person"]');  //получить все такие формы
+console.log(searchPerson3);
