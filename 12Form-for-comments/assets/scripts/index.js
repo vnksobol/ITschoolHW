5ff548368ad71657303493c1868b2752cef69f7f
vnksobol <vnksobol@gmail.com>
2023-04-10 23:39:14 +0300

    index filled

--- 12Form-for-comments/assets/scripts/index.js
+++ 12Form-for-comments/assets/scripts/index.js
@@ -1,1 +1,3 @@
-
+// Получить введенные пользователем данные, преобразовать в строку
+const getUserName = document.querySelector('.user-name').textContent;
+const getUserComment = document.querySelector('.user-comment').textContent;
