19aa517753c80d04ad558c4c49b37c83d797cb42
vnksobol <vnksobol@gmail.com>
2023-04-23 23:43:36 +0300

    first commit

---
+++ 14Arrays/assets/scripts/index.js
@@ -0,0 +1,18 @@
+let mas = new Array(2);
+mas[3] = 5;
+console.log(mas[3]);
+
+// let masAnother = new Array(2);
+// masAnother[3] = 5;
+// console.log(mas3);
+
+let a = new Array(2);
+a[1] = null;
+console.log( a.length );
+
+// const students = [“Lena", “Olya", "Ylia", "Roma" ,  "Vova" ];
+//  console.log(students[3]);
+
+const fruits = [ "Груша", "Яблоко", "Лимон", "Ананас",];
+console.log(fruits.shift()); 
+console.log(fruits);
