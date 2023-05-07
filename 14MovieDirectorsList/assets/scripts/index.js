6976af48bf042f3bc9fa309d0864f4461dea52bd
vnksobol <vnksobol@gmail.com>
2023-05-07 23:34:34 +0300

    add second array

--- 14MovieDirectorsList/assets/scripts/index.js
+++ 14MovieDirectorsList/assets/scripts/index.js
@@ -1,4 +1,4 @@
-// + Сохранить в переменную массив.
+// Сохранить в переменную массив.
 const getList = [
     {
         name: 'Стивен Спилберг',
@@ -43,23 +43,12 @@ const getList = [
         top_rated_film: 'Далласский клуб покупателей'
     },
 ];
-// console.log(favouriteDirectors);
+// console.log(getList);
 
-// Разметка как образец:
-// <div class="director">
-//   <div class="director__wrapp-name">
-//     <p class="director__name">Питер Фаррелли</p>
-//     <p class="director__career">Продюсер, Режиссёр, Сценарист, Актёр</p>
-//   </div>
-//   <div class="director__wrapp-movie">
-//   <a href="https://www.film.ru/person/zhan-mark-valle" class="director__movie"> ссылка на фильмы </a>
-//   </div>
-// </div>;
-
-const finalList = document.querySelector('.director');
+const directorsList = document.querySelector('.director');
 
 // В найденные элементы на странице добавить информацию из каждого элемента массива.
-// Создаем элементы при помощи функции, размещаем при помощи forEach.
+// Создать элементы при помощи функции, разместить при помощи forEach.
 
 const createElement = (element, i) => {
     const divDirector = document.createElement("div");
@@ -75,11 +64,9 @@ const createElement = (element, i) => {
     const pCareer = document.createElement("p");
     pCareer.className = "director__career";
     pCareer.textContent = `${element.career}`;
-    
     const divWrapperMovie = document.createElement("div");
     divWrapperMovie.className = "director__wrapper-movie";
     const movie = document.createElement("a");
-    // movie.className = "director__movie";
     movie.textContent = "фильмография";
     movie.href = `${element.films}`;
     movie.target = "_blank";
@@ -90,22 +77,33 @@ const createElement = (element, i) => {
     return divDirector;
 };
 
-// // - В функцию обработчик добавить код, который относится к работе с DOM.
+// В функцию-обработчик добавить код, который относится к работе с DOM.
 getList.forEach((item, index) => {
-    const toDom = createElement(item, index);
-    finalList.append(toDom);
+    const directorsDom = createElement(item, index);
+    directorsList.append(directorsDom);
     // console.log(item.name);
-    // console.log(item.career);
-    // console.log(item.movies);
 });
 
+const bestMoviesList = document.querySelector('.best-movies');
 
-const topFilmsList = [
+// Создать новый массив `topFilmsList`, в нём - лучшие фильмы режиссёров
+// Создать новый массив на основе существующего
+const topFilmsList = getList.map(function (item) { 
+    return item.top_rated_film;
+}); 
+// console.log(topFilmsList);
 
-];
+// Создать новый элемент разметки для списка фильмов
+const createElement2 = (element, i) => {
+    const divTopFilm = document.createElement("div");
+    divTopFilm.innerHTML = element;
+    divTopFilm.className = "best-movies";
+    
+    return divTopFilm;
+};
 
-
-// // - Создать новый массив `topFilmsList`, в нём должны быть лучшие фильмы режиссёров (хранятся в полях `top_rated_film`).
-// movies.top_rated_film;
-//  = [];
-// topFilmsList.push(movies);
+// В функцию-обработчик добавить код, который относится к работе с DOM.
+topFilmsList.forEach((item, index) => {
+    const topFilmDom = createElement2(item, index);
+    bestMoviesList.append(topFilmDom);
+});
